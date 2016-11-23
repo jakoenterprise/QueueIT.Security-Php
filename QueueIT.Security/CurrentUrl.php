f5ada495a0e1ea4b7994a04af5f5b747b5c12189
Michael Giuliana <michael.giuliana@gmail.com>
2016-11-22 23:49:24 -0500

    use `HTTP_HOST` rather than `SERVER_NAME`

--- QueueIT.Security/CurrentUrl.php
+++ QueueIT.Security/CurrentUrl.php
@@ -7,9 +7,9 @@ function currentUrl()
 	if ($ssl) {$pageURL .= "s";}
 	$pageURL .= "://";
 	if ((!$ssl && $_SERVER["SERVER_PORT"] != "80") || ($ssl && $_SERVER["SERVER_PORT"] != "443"))  {
-		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+		$pageURL .= $_SERVER["HTTP_HOST"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
 	} else {
-		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+		$pageURL .= $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
 	}
 	return $pageURL;
 }
